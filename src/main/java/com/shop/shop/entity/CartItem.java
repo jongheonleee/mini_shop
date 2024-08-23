ec56fc4f28cc4a6fc9132bb0c649f750a3988e4d
jongheonleee <qwefghnm1212@gmail.com>
2024-08-23 23:30:52 +0900

    de

---
+++ src/main/java/com/shop/shop/entity/CartItem.java
@@ -0,0 +1,33 @@
+package com.shop.shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name="cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name="cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    private int count;
+
+}
---
+++ src/test/java/com/shop/shop/entity/CartTest.java
@@ -0,0 +1,65 @@
+package com.shop.shop.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.shop.shop.dto.MemberFormDto;
+import com.shop.shop.repository.CartRepository;
+import com.shop.shop.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class CartTest {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMember() {
+        var memberFormDto = new MemberFormDto();
+
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 강남구");
+        memberFormDto.setPassword("1234");
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("장바구니 회원 엔티티 매핑 조회 테스트")
+    public void findCartAndMemberTest() {
+        var member = createMember();
+        memberRepository.save(member);
+
+        var cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        em.flush();
+        em.close();
+
+        Cart savedCart = cartRepository.findById(cart.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        assertEquals(savedCart.getMember().getId(), member.getId());
+    }
+}
